086dc4cc7845524a42e7e3215f6240cfdb1ed1fe
dianfrf <arifdian56@gmail.com>
2020-12-07 10:09:26 +0700

    Edit UI v_artist

--- application/views/front/v_artists.php
+++ application/views/front/v_artists.php
@@ -2,15 +2,17 @@
     <div class="container">
         <h1>ARTISTS</h1>
         <!-- Show All Artists -->
-        <div class="baris">
+        <div class="row rowartis">
             <?php foreach($artists as $a) { ?>
-            <div class="column colartis">
-                <a href="<?=base_url()?>Artist_Detail/<?=$a->id_artist?>">
-                    <img src="<?=base_url()?>Asset/img/artists/<?=$a->picture?>" class="lazyload">
-                    <div class="artistoverlay">
+            <div class="col-md-3 colartis">
+                <div class="card">
+                    <a href="<?=base_url()?>Artist_Detail/<?=$a->id_artist?>">
+                        <img src="<?=base_url()?>Asset/img/artists/<?=$a->picture?>" class="lazyload">
+                    </a> 
+                    <div class="card-body">
                         <div class="artistname"><strong><?=$a->name?></strong></div>
-                    </div>
-                </a>
+                    </div>   
+                </div>
             </div>
             <?php } ?>
         </div>
